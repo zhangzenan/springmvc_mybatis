3469619cb9e2709b3a7d53b9d6f71367938f69dc
zhangzenan <zzn1991129@126.com>
2017-08-05 23:02:32 +0800

    restful风格

--- src/cn/itcast/ssm/controller/ItemsController.java
+++ src/cn/itcast/ssm/controller/ItemsController.java
@@ -16,9 +16,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -127,6 +129,15 @@ public class ItemsController {
 		model.addAttribute("items",itemsCustom);
 
 		return "items/editItems";
+	}
+	
+	//查询商品信息，输出json
+	///itemsView/{id}里边的{id}表示这个位置的参数传到@PathVariable指定的名称中。
+	@RequestMapping("/itemsView/{id}")
+	public @ResponseBody ItemsCustom itemsView(@PathVariable("id") Integer id) throws Exception{
+		//调用service查询商品信息
+		ItemsCustom itemsCustom= itemsService.findItemsById(id);
+		return itemsCustom;
 	}
 
 	/**
